82eee622562a5d34ae765096e00f274331edd988
Ahsan <m.ahsankhan26@hotmail.com>
2021-01-03 03:06:22 +0500

    Executes Function on Enter and Deletes old InputTag and adds another

--- script.js
+++ script.js
@@ -1,7 +1,45 @@
 let input = document.querySelector("input");
 let terminalBody = document.querySelector("#terminalBody");
 
-terminalBody.addEventListener ('click', function(event){
-    console.log("clicked!");
+
+let string = {
+    name: "Ahsan Khan"
+}
+
+functionCalls();
+
+function functionCalls() {
+    checkWindowClick();
+    checkPressedEnter();
+}
+
+function checkWindowClick() {
+    terminalBody.addEventListener ('click', function(event){
+        // console.log("clicked!");
+        input.focus();
+    });
+}
+
+function checkPressedEnter() {
+    input.addEventListener('keydown', function(e){
+        // console.log("function");
+        if (e.key === 'Enter') {
+            // console.log("Enter");
+            execute(input.value);
+        }
+    });
+}
+
+function execute(inputValue) {
+
+    input.remove();
+    terminalBody.innerHTML += "<br>Hello";
+    addInput();
+}
+
+function addInput() {
+    terminalBody.innerHTML += '<input type="text" autofocus />';
+    input = document.querySelector("input");
     input.focus();
-})
+    functionCalls();
+}
